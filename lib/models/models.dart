d5e3b9e5cabfb76969d07e5844a9edb1ff4abb14
HusniFadhilah <husnfadhilah62@gmail.com>
2020-12-24 08:56:36 +0700

    Memperbaiki state cubit post

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,5 +1,6 @@
 // import 'package:equatable/equatable.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 // import 'package:flutter/material.dart';
 
 part 'komentar.dart';
